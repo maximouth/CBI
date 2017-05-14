f6ba1c69e1227adbb28138af86afbc1116864c75
Myself <maximouth@localhost.localdomain>
2017-05-14 16:40:30 -0400

    Beautifuer of the code

--- Devel/IXL/Simul/main.ml
+++ Devel/IXL/Simul/main.ml
@@ -3,11 +3,11 @@ open Utils_simul ;;
 open Parsing;;
 open Lexer_simul;;
 open Simulation;;
-
+  
 (* Storage of the input and output files *)
 let in_Simul_channel = ref stdin ;;
 let out_Simul_channel = ref stdout ;;
-
+  
 (* Parse of a simulation file *) 
 let parse_simul in_Simul_channel =
   (* Initialize the lexer channel *)
@@ -18,50 +18,50 @@ let parse_simul in_Simul_channel =
       let cycles = Parser_simul.toplevel_phrase
 		     Lexer_simul.token current_expr_lexbuf
       in 
-	  (* Return the parsed equations *)
-	  cycles
+      (* Return the parsed equations *)
+      cycles
 	(* Management of the exceptions *)
     with
-      | Parsing.Parse_error -> 
-	      Format.printf "Syntax error at line '%i' @." !Lexer_simul.line; exit 0
-      | Lexer_simul.LexError -> 
-	      Format.printf "Lexical error at line '%i' @." !Lexer_simul.line; exit 0
-      | Lexer_simul.Unterminated_comment -> 
-	      Format.printf "Unterminated comment@." ; exit 0
-      | End_of_file -> exit 0
+    | Parsing.Parse_error -> 
+       Format.printf "Syntax error at line '%i' @." !Lexer_simul.line; exit 0
+    | Lexer_simul.LexError -> 
+       Format.printf "Lexical error at line '%i' @." !Lexer_simul.line; exit 0
+    | Lexer_simul.Unterminated_comment -> 
+       Format.printf "Unterminated comment@." ; exit 0
+    | End_of_file -> exit 0
   end;
 ;;
-
+  
 let mainloop () =
-	let cycles = 
-	  (* check that there is a file as input *)
-	  if (!in_Simul_channel <> stdin) then
-	     (* parse the file *)
-	     parse_simul !in_Simul_channel
-	  else
-	  begin
-	    Format.printf "option -i  mandatory.\n";
-		exit(0)
-          end
-	in
-	(* Print the parsed file, for debug purpose only *)
-        (* Utils_simul.print_cycles cycles; *)
-	
-	(* Generate the VHDL from the simulation *)
-	Simulation.generate !out_Simul_channel cycles;
-	
-	(* Close the output file *)
-	close_out !out_Simul_channel
+  let cycles = 
+    (* check that there is a file as input *)
+    if (!in_Simul_channel <> stdin) then
+      (* parse the file *)
+      parse_simul !in_Simul_channel
+    else
+      begin
+	Format.printf "option -i  mandatory.\n";
+	exit(0)
+      end
+  in
+  (* Print the parsed file, for debug purpose only *)
+  (* Utils_simul.print_cycles cycles; *)
+  
+  (* Generate the VHDL from the simulation *)
+  Simulation.generate !out_Simul_channel cycles;
+  
+  (* Close the output file *)
+  close_out !out_Simul_channel
 ;;
-
+  
 Arg.parse 
- [("-i", 
-   Arg.String(fun f_in_name -> in_Simul_channel := open_in f_in_name;),
-   " : Simulation input file");
-  ("-o", 
-   Arg.String(fun f_out_name -> out_Simul_channel := open_out f_out_name;),
-   " : Simulation output file");
- ]
+  [("-i", 
+    Arg.String(fun f_in_name -> in_Simul_channel := open_in f_in_name;),
+    " : Simulation input file");
+   ("-o", 
+    Arg.String(fun f_out_name -> out_Simul_channel := open_out f_out_name;),
+    " : Simulation output file");
+  ]
   (fun f_other -> ())
   "Glop pas glop..." ;
 
@@ -70,3 +70,4 @@ try
 with
 | Failure explanation -> Printf.fprintf stderr "%s" explanation 
 ;;
+  
--- Devel/IXL/Simul/simulation.ml
+++ Devel/IXL/Simul/simulation.ml
@@ -1,8 +1,8 @@
 open Simul_t ;;
-
+  
 (* Print the header of the VHDL file *)
 let print_header out_channel =
-Printf.fprintf out_channel 
+  Printf.fprintf out_channel 
 "-- type declaration in a package
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -79,7 +79,7 @@ begin
 
 (* Print the trailer of the VHDL file *)
 let print_trailer out_channel =
-Printf.fprintf out_channel
+  Printf.fprintf out_channel
 "    
     wait;
   end process;
@@ -89,94 +89,94 @@ end simu;
 
 (* Print one event *)
 let print_ident_in ident =
-match ident with
-| Simul.P_SE(nb, _, dir) -> 
-  begin
-  match dir with
-  | Simul.Up   -> "      Sensor("^string_of_int(nb-1)^").dir <= \"01\";\n"
-  | Simul.Down -> "      Sensor("^string_of_int(nb-1)^").dir <= \"10\";\n"
-  | Simul.Idle -> "      Sensor("^string_of_int(nb-1)^").dir <= \"00\";\n"
-  end
-| Simul.P_TC(nb, _)      -> raise( Failure "?*&%$!! Error TC not an input" )
-| Simul.P_SW_CMD(nb, st) ->
-  begin
-  match st with 
-  | Simul.Right -> "      Sw_Cmd_Req("^string_of_int((nb-1)*2)^") <='1';\n"
-  | Simul.Left  -> "      Sw_Cmd_Req("^string_of_int((nb-1)*2+1)^") <='1';\n"
-  | Simul.Idle  -> "      Sw_Cmd_req("^string_of_int((nb-1)*2)^") <= '0';
-      Sw_Cmd_Req("^string_of_int((nb-1)*2+1)^") <= '0';\n"
-  end
-| Simul.P_SW_ST(nb, st)  -> 
-  begin
-  match st with 
-  | Simul.Right -> "      Sw_State("^string_of_int((nb-1)*2)^") <= '1';\n"
-  | Simul.Left  -> "      Sw_State("^string_of_int((nb-1)*2+1)^") <= '1';\n"
-  | Simul.Idle  -> "      Sw_State("^string_of_int((nb-1)*2)^") <= '0';
-      Sw_State("^string_of_int((nb-1)*2+1)^") <= '0';\n"
-  end
-| Simul.P_SW_AUT(nb, st) -> raise( Failure "!@*&* Error SW_AUT never in" )
+  match ident with
+  | Simul.P_SE(nb, _, dir) -> 
+     begin
+       match dir with
+       | Simul.Up   -> "      Sensor("^string_of_int(nb-1)^").dir <= \"01\";\n"
+       | Simul.Down -> "      Sensor("^string_of_int(nb-1)^").dir <= \"10\";\n"
+       | Simul.Idle -> "      Sensor("^string_of_int(nb-1)^").dir <= \"00\";\n"
+     end
+  | Simul.P_TC(nb, _)      -> raise( Failure "?*&%$!! Error TC not an input" )
+  | Simul.P_SW_CMD(nb, st) ->
+     begin
+       match st with 
+       | Simul.Right -> "      Sw_Cmd_Req("^string_of_int((nb-1)*2)^") <='1';\n"
+       | Simul.Left  -> "      Sw_Cmd_Req("^string_of_int((nb-1)*2+1)^") <='1';\n"
+       | Simul.Idle  -> "      Sw_Cmd_req("^string_of_int((nb-1)*2)^") <= '0';
+								     Sw_Cmd_Req("^string_of_int((nb-1)*2+1)^") <= '0';\n"
+     end
+  | Simul.P_SW_ST(nb, st)  -> 
+     begin
+       match st with 
+       | Simul.Right -> "      Sw_State("^string_of_int((nb-1)*2)^") <= '1';\n"
+       | Simul.Left  -> "      Sw_State("^string_of_int((nb-1)*2+1)^") <= '1';\n"
+       | Simul.Idle  -> "      Sw_State("^string_of_int((nb-1)*2)^") <= '0';
+								   Sw_State("^string_of_int((nb-1)*2+1)^") <= '0';\n"
+     end
+  | Simul.P_SW_AUT(nb, st) -> raise( Failure "!@*&* Error SW_AUT never in" )
 ;;
-
-
+  
+  
 (* Print the event list *)
 let rec print_events c =
-match c with
-| [] -> ""
-| e::t ->
-  let s = print_ident_in e.Simul.evname
-  in s^(print_events t)
+  match c with
+  | [] -> ""
+  | e::t ->
+     let s = print_ident_in e.Simul.evname
+     in s^(print_events t)
 ;;
-
-
+  
+  
 let print_ident_out ident =
-match ident with
-| Simul.P_SE(nb, _, dir) ->  raise( Failure "!@*&* Error TC never in")
-| Simul.P_TC(nb, _)      -> "TC_out("^string_of_int(nb-1)^")"
-| Simul.P_SW_CMD(nb, st) ->  raise( Failure "!@*&* Error SW_CMD never out" )
-| Simul.P_SW_ST(nb, st)  ->  raise( Failure "!@*&* Error SW_ST never in" )
-| Simul.P_SW_AUT(nb, st) ->
-  begin
-  match st with 
-    | Simul.Right -> "Sw_Cmd_Aut("^string_of_int((nb-1)*2)^")"
-    | Simul.Left ->  "Sw_Cmd_Aut("^string_of_int((nb-1)*2 + 1)^")"
-    | Simul.Idle -> raise( Failure "*&!%$ Error Sw_Aut Idle never out." )
-  end
+  match ident with
+  | Simul.P_SE(nb, _, dir) ->  raise( Failure "!@*&* Error TC never in")
+  | Simul.P_TC(nb, _)      -> "TC_out("^string_of_int(nb-1)^")"
+  | Simul.P_SW_CMD(nb, st) ->  raise( Failure "!@*&* Error SW_CMD never out" )
+  | Simul.P_SW_ST(nb, st)  ->  raise( Failure "!@*&* Error SW_ST never in" )
+  | Simul.P_SW_AUT(nb, st) ->
+     begin
+       match st with 
+       | Simul.Right -> "Sw_Cmd_Aut("^string_of_int((nb-1)*2)^")"
+       | Simul.Left ->  "Sw_Cmd_Aut("^string_of_int((nb-1)*2 + 1)^")"
+       | Simul.Idle -> raise( Failure "*&!%$ Error Sw_Aut Idle never out." )
+     end
 ;;
-
+  
 (* Print the output list *)
 let rec print_outputs c =
-match c with
-| [] -> "\n"
-| o::t ->
-  let value = if o.Simul.outval then "\'1\'" else "\'0\'"
-  in
-  let id = print_ident_out o.Simul.outname
-  in
-  let s = match o.Simul.comment with
-  | None -> ""
-  | Some co -> 
-     "      if ("^id^" = "^value^") then report \""^co^"\" & \" : Pass\"; else report \""^co^"\" & \" : Fail.\"; end if;\n"
-  in 
-  s^(print_outputs t)
+  match c with
+  | [] -> "\n"
+  | o::t ->
+     let value = if o.Simul.outval then "\'1\'" else "\'0\'"
+     in
+     let id = print_ident_out o.Simul.outname
+     in
+     let s = match o.Simul.comment with
+       | None -> ""
+       | Some co -> 
+	  "      if ("^id^" = "^value^") then report \""^co^"\" & \" : Pass\"; else report \""^co^"\" & \" : Fail.\"; end if;\n"
+     in 
+     s^(print_outputs t)
 ;;
-
-
+  
+  
 (* Generate one cycle:
    - generate the event of the current cycle (cc)
    - rising edge of the clock (start cycle)
    - check the outputs of the current cycle (cc)
    - falling of the clock
-*)
+ *)
 let generate_cycle out_channel cc =
   let outputs = print_outputs cc.Simul.outputs in
   let events = print_events cc.Simul.events in
-
+  
   begin
-  match cc.Simul.comment with
-  | None -> Printf.fprintf out_channel "      report \"Cycle %i:\";\n" cc.Simul.cycle
-  | Some co -> Printf.fprintf out_channel "      report \"Cycle %i: %s\";\n" cc.Simul.cycle co;
+    match cc.Simul.comment with
+    | None -> Printf.fprintf out_channel "      report \"Cycle %i:\";\n" cc.Simul.cycle
+    | Some co -> Printf.fprintf out_channel "      report \"Cycle %i: %s\";\n" cc.Simul.cycle co;
   end;
-
+  
   Printf.fprintf out_channel "%s" events;
   Printf.fprintf out_channel "
       CLK <= '1';
@@ -192,20 +192,20 @@ let generate_cycle out_channel cc =
 
 (* Generate the cycles *)
 let rec generate_cycles out_channel cycles =
-match cycles with
-| [] -> ()
-| c::t -> 
-   generate_cycle out_channel c;
-   generate_cycles out_channel t
+  match cycles with
+  | [] -> ()
+  | c::t -> 
+     generate_cycle out_channel c;
+     generate_cycles out_channel t
 ;;
-
-
+  
+  
 let generate out_channel cycles =
   print_header out_channel;
   generate_cycles out_channel cycles; 
   print_trailer out_channel
 ;;
- 
+  
 
 
 
